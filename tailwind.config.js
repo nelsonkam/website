30a2143581efc277aba7ab10eb7d110a436e2602
nelson <kamganelson@gmail.com>
2026-03-30 13:03:06 -0400

    remove all files

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import prose from "@tailwindcss/typography";
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [".vitepress/**/*.vue", "./**/*.{md,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [prose],
-};
